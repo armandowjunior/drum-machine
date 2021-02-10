cd7dc4c883a79b88006d74ce4eff08ff89ac07b1
Armando Wilmans <armandowjunior@gmail.com>
2021-02-10 00:38:26 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Drum from "./components/Drum-UI";
 
 function App() {
+  const [display, setDisplay] = useState("Press a Key!");
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => document.removeEventListener("keydown", handleKeyPress);
+  }, []);
+
+  function handleKeyPress(event) {
+    if (event.keyCode === 81) {
+      document.getElementById("Heater-1").click();
+    }
+    if (event.keyCode === 87) {
+      document.getElementById("Heater-2").click();
+    }
+    if (event.keyCode === 69) {
+      document.getElementById("Heater-3").click();
+    }
+    if (event.keyCode === 65) {
+      document.getElementById("Heater-4").click();
+    }
+    if (event.keyCode === 83) {
+      document.getElementById("Clap").click();
+    }
+    if (event.keyCode === 68) {
+      document.getElementById("Open-HH").click();
+    }
+    if (event.keyCode === 90) {
+      document.getElementById("Kick-n'-Hat").click();
+    }
+    if (event.keyCode === 88) {
+      document.getElementById("Kick").click();
+    }
+    if (event.keyCode === 67) {
+      document.getElementById("Closed-HH").click();
+    }
+  }
+
+  const handleClick = (id, str) => () => {
+    // Returns a function that executes another function
+    const sound = document.getElementById(id);
+    setDisplay(str);
+    sound.currentTime = 0;
+    sound.play();
+    sound.parentNode.classList.add("active");
+    setTimeout(() => {
+      sound.parentNode.classList.remove("active");
+    }, 100);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Drum display={display} handleClick={handleClick} />
     </div>
   );
 }
---
+++ src/components/Drum-UI.js
@@ -0,0 +1,131 @@
+import React from "react";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faDrum } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faDrum);
+
+const Drum = (props) => {
+  return (
+    <div id="drum-machine">
+      <p id="title">
+        <span>
+          <FontAwesomeIcon icon="drum" />
+        </span>
+        Drum Machine
+      </p>
+      <h2 id="display">{props.display}</h2>
+      <button
+        className="drum-pad"
+        id="Heater-1"
+        onClick={props.handleClick("Q", "Heater 1")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+          id="Q"
+          className="clip"
+        ></audio>
+        Q
+      </button>
+      <button
+        className="drum-pad"
+        id="Heater-2"
+        onClick={props.handleClick("W", "Heater 2")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+          id="W"
+          className="clip"
+        ></audio>
+        W
+      </button>
+      <button
+        className="drum-pad"
+        id="Heater-3"
+        onClick={props.handleClick("E", "Heater 3")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+          id="E"
+          className="clip"
+        ></audio>
+        E
+      </button>
+      <button
+        className="drum-pad"
+        id="Heater-4"
+        onClick={props.handleClick("A", "Heater 4")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+          id="A"
+          className="clip"
+        ></audio>
+        A
+      </button>
+      <button
+        className="drum-pad"
+        id="Clap"
+        onClick={props.handleClick("S", "Clap")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+          id="S"
+          className="clip"
+        ></audio>
+        S
+      </button>
+      <button
+        className="drum-pad"
+        id="Open-HH"
+        onClick={props.handleClick("D", "Open HH")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+          id="D"
+          className="clip"
+        ></audio>
+        D
+      </button>
+      <button
+        className="drum-pad"
+        id="Kick-n'-Hat"
+        onClick={props.handleClick("Z", "Kick n' Hat")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+          id="Z"
+          className="clip"
+        ></audio>
+        Z
+      </button>
+      <button
+        className="drum-pad"
+        id="Kick"
+        onClick={props.handleClick("X", "Kick")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+          id="X"
+          className="clip"
+        ></audio>
+        X
+      </button>
+      <button
+        className="drum-pad"
+        id="Closed-HH"
+        onClick={props.handleClick("C", "Closed HH")}
+      >
+        <audio
+          src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+          id="C"
+          className="clip"
+        ></audio>
+        C
+      </button>
+    </div>
+  );
+};
+
+export default Drum;
